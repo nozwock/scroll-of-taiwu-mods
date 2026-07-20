4d77967464d9b28cd1ee1d7c0929fa56ac13edd8
nozwock <57829219+nozwock@users.noreply.github.com>
2026-07-20 19:00:41 +0530

    Add patch to remove male NPCs with female portraits and vice-versa
    Alters `_transgender` and `Avatar.Gender` state destructively since it's
    easier to implement this way. State cannot be restored back after
    disabling the patch.

---
+++ Tweaks/Backend/Patches/NoTransgender.cs
@@ -0,0 +1,53 @@
+using GameData.Domains.Character;
+using HarmonyLib;
+
+namespace Tweaks.Patches;
+
+// Destructive. Changes are persisted and transgender and avatar gender state cannot be restored by disabling the patch.
+[HarmonyPatch]
+static class PatchNoTransgender
+{
+    // Patches for all different constructors/initializers
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Character), nameof(Character.OfflineSetGenderInfo))]
+    static void Character_OfflineSetGenderInfo_Prefix(ref bool transgender) => transgender = false;
+
+    // This is when creating a new character I think?
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Character), nameof(Character.OfflineCreateProtagonist))]
+    static void Character_OfflineCreateProtagonist_Postfix(Character __instance)
+    {
+        var self = __instance;
+        // Prioritizing inscribed character's portrait for gender stat
+        if (self._transgender)
+        {
+            self._transgender = false;
+            self._gender = self._avatar.Gender;
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Character), MethodType.Constructor, [typeof(short)])]
+    [HarmonyPatch(typeof(Character), nameof(Character.OfflineInheritCrossArchiveCharacter))]
+    static void SimpleNoTransgender_Postfix(Character __instance) =>
+        __instance._transgender = false;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Character), nameof(Character.Deserialize))]
+    static void NoTransgender_Postfix(Character __instance) => RemoveTransgender(__instance);
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Character), nameof(Character.Serialize))]
+    [HarmonyPatch(typeof(Character), nameof(Character.GetAvatar))]
+    [HarmonyPatch(typeof(Character), nameof(Character.OfflineCreateAttractionAndAvatar))]
+    static void NoTransgender_Prefix(Character __instance) => RemoveTransgender(__instance);
+
+    static void RemoveTransgender(Character character)
+    {
+        if (character._transgender)
+        {
+            character._transgender = false;
+            character._avatar.ChangeGender(character._gender);
+        }
+    }
+}
