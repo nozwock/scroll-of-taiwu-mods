8db20a9744f5d9e18e6d4f5650bc05859338c5bc
nozwock <57829219+nozwock@users.noreply.github.com>
2026-07-20 18:33:25 +0530

    Add patch to make unrepairable items always have max durability
    Items such as craft tools and accessories (relics).

---
+++ Tweaks/Backend/Patches/MaxItemDurability.cs
@@ -0,0 +1,21 @@
+using GameData.Domains.Item;
+using HarmonyLib;
+
+namespace Tweaks.Patches;
+
+[HarmonyPatch]
+static class PatchMaxItemDurability
+{
+    // CraftTool and Accessory because they're not repairable
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CraftTool), nameof(CraftTool.SetCurrDurability))]
+    [HarmonyPatch(typeof(Accessory), nameof(Accessory.SetCurrDurability))]
+    static void Partial_ItemBase_SetCurrDurability_Prefix(
+        object __instance,
+        ref short currDurability
+    )
+    {
+        var item = (ItemBase)__instance;
+        currDurability = item.GetMaxDurability();
+    }
+}
