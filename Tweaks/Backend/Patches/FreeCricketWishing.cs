4101bf9869134aaa681d0e516ac0eb6b306b601d
nozwock <57829219+nozwock@users.noreply.github.com>
2026-07-22 09:06:18 +0530

    Fix _cricketLuckPoint being allowed to be set to negative value

--- Tweaks/Backend/Patches/FreeCricketWishing.cs
+++ Tweaks/Backend/Patches/FreeCricketWishing.cs
@@ -12,4 +12,13 @@ static class PatchFreeCricketWishing
     {
         GlobalConfig.Instance.CricketWishingCostLuckPoint = 0;
     }
+
+    // Fix _cricketLuckPoint being allowed to be set to negative value
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(TaiwuDomain), nameof(TaiwuDomain.SetCricketLuckPoint))]
+    static void TaiwuDomain_SetCricketLuckPoint_Prefix(ref int value)
+    {
+        if (value < 0)
+            value = 0;
+    }
 }
