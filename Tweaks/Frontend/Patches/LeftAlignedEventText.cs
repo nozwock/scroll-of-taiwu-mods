8ec81b5101ed100cd7517d8150b05c332508ed59
nozwock <57829219+nozwock@users.noreply.github.com>
2026-07-29 01:12:27 +0530

    Add patch to left align the center aligned event text

---
+++ Tweaks/Frontend/Patches/LeftAlignedEventText.cs
@@ -0,0 +1,15 @@
+using Game.Views.EventWindow;
+using HarmonyLib;
+
+[HarmonyPatch]
+static class PatchLeftAlignedEventText
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ViewEventWindow), nameof(ViewEventWindow.Awake))]
+    static void ViewEventWindow_Awake_Postfix(ViewEventWindow __instance)
+    {
+        var self = __instance;
+        // It is center by default. At least it is so at the moment of writing.
+        self.eventContent.alignment = TMPro.TextAlignmentOptions.Left;
+    }
+}
