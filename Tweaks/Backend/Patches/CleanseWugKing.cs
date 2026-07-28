06ce2906daa9ffab4691785cdee284a434fd7a76
nozwock <57829219+nozwock@users.noreply.github.com>
2026-07-28 20:23:53 +0530

    Add patch to cleanse Prime Gu using 5 Immortal sect's function
    Five Immortal sect's function only reduces the duration for how long the
    Prime Gu will be in the host by 12 months. The patch will have the Prime
    Gu killed instead.

---
+++ Tweaks/Backend/Patches/CleanseWugKing.cs
@@ -0,0 +1,26 @@
+using GameData.Domains.Character;
+using GameData.Domains.TaiwuEvent.EventHelper;
+using HarmonyLib;
+
+// Wug King -> Prime Gu
+// Makes it so, using the sect function also removes any Prime Gu
+[HarmonyPatch]
+static class PatchCleanseWugKing 
+{
+    // EventHelper.ApplyEffect_WuShengMiYu - The sect function of 5 Immortals Sect
+    //      EventHelper.ChangeWugKingDurations
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(EventHelper), nameof(EventHelper.ChangeWugKingDurations))]
+    static void EventHelper_ChangeWugKingDurations_Prefix(ref short deltaDuration)
+    {
+        deltaDuration = short.MinValue;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Character), nameof(Character.ChangeFeatureByWugKing))]
+    static bool Character_ChangeFeatureByWugKing_Prefix(ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+}
