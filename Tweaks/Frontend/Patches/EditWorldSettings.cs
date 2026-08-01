6ba8259d2eddd220feb49fb82ae6e3015b6bc0ca
nozwock <57829219+nozwock@users.noreply.github.com>
2026-08-01 10:15:24 +0530

    Update confirm button label in PatchEditWorldSettings to be more correct
    Now, "Modify the World Details" instead of "Taiwu Inheritance".

--- Tweaks/Frontend/Patches/EditWorldSettings.cs
+++ Tweaks/Frontend/Patches/EditWorldSettings.cs
@@ -1,5 +1,10 @@
+using System.Reflection;
+using Common.Extensions;
+using Game.Views.LegacyPassing;
 using GameData.GameDataBridge;
+using GameData.Utilities;
 using HarmonyLib;
+using TMPro;
 
 namespace Tweaks.Patches;
 
@@ -23,5 +28,31 @@ static class PatchEditWorldSettings
     {
         __result = true;
         return false;
+    }
+
+    // Updating the text for ViewLegacy.confirm in ViewLegacy.OnInit() doesn't work since Element.ShowAfterRefresh()
+    // inside ViewLegacy.RequestData() refreshes the UI with translated texts.
+    [HarmonyPatch]
+    static class ViewLegacy_RequestData_delegate_Postfix
+    {
+        static MethodBase TargetMethod() =>
+            typeof(ViewLegacy).GetLocalMethod(
+                $"<{nameof(ViewLegacy.RequestData)}>",
+                [typeof(int), typeof(RawDataPool)],
+                isRootMethod: true
+            );
+
+        static void Postfix(ViewLegacy __instance)
+        {
+            var self = __instance;
+
+            var isManuallyOpened = !self._inherit && !self._crossArchive;
+            if (isManuallyOpened)
+            {
+                // Or UI_Reset_World_Config_Title
+                self.confirm.GetComponentInChildren<TextMeshProUGUI>().text =
+                    LanguageKey.GM_EditWorldCreationInfo_Name.Tr();
+            }
+        }
     }
 }
